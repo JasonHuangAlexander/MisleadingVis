4fdc41ecf478ce8994241391f1076f98ec4f24bf
JasonHuangAlexander <89747019+JasonHuangAlexander@users.noreply.github.com>
2023-10-15 16:43:05 -0400

    Update MisleadingVis redone OCR code.py

--- MisleadingVis redone OCR code.py
+++ MisleadingVis redone OCR code.py
@@ -1,1 +1,8 @@
-print("OK")
+from PIL import Image
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+test = Image.open("C:\Jason Alexander\MisleadingVIs\COVID vs flu cases, 2020 1.png")
+#test.show()
+print(pytesseract.image_to_string(test))
+
